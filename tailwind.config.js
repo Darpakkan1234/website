181d2b56ebfc46cb9e5cc893dc65c49254db37c6
Darpakkan <147263578+Darpakkan@users.noreply.github.com>
2023-11-06 17:50:44 +0530

    Push Again maybe it will fix deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: ["./src/**/*.{html,js}", "./docs/**/*.{html,js}"],
   content: ["./src/**/*.{html,js}", "./docs/**/*.{html,js}"],
   theme: {
     extend: {},
